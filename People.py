a050efc0de9244b7b7695c56e0734131a3be68fc
SoylentGreen <andrewrmccoy@gmail.com>
2019-01-06 15:40:47 -0600

    Added params to person class

--- People.py
+++ People.py
@@ -1,2 +1,9 @@
 class Person:
-    def __init__(self, name, level, stats{}):
+
+    stats = {'strength': 1, 'agility': 1, 'charm': 1}
+
+    def __init__(self, name, level, stats):
+        self.name = name
+        self.level = level
+        self.stats = stats
+
